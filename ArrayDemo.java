53d736cc8e31da08c1367806e9b6b660715e5d54
Michael Chan <mchan10@stuy.edu>
2018-09-27 17:02:57 -0400

    created printArray

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,7 +1,11 @@
 public class ArrayDemo{
-    public static void main(String[] args){
-        public static void printArray(int[]ary){
-			
-		}	
-    }
-}
+    public static void main(String[] args){	
+		int[] array = new int[5];
+		printArray(array);
+	}
+	public static void printArray(int[]ary){
+		for (int i = 0; i < ary.length; i += 1){
+			System.out.print(ary[i] + ",");
+		}
+	}
+}	
