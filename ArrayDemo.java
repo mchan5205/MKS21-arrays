8db08a46df28e6c5bd48846d5fe7be12995de4f5
Michael Chan <mchan10@stuy.edu>
2018-09-27 19:28:39 -0400

    added printArray for 2D arrays

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -2,10 +2,18 @@ public class ArrayDemo{
     public static void main(String[] args){	
 		int[] array = new int[5];
 		printArray(array);
+		int[][] array2 = new int[3][3];
+		printArray(array2);
 	}
 	public static void printArray(int[]ary){
 		for (int i = 0; i < ary.length; i += 1){
 			System.out.print(ary[i] + ",");
-		}
+		}	
+		System.out.println("");
 	}
+	public static void printArray(int[][]ary){
+		for (int x = 0; x < ary.length; x += 1){
+			printArray(ary[x]);
+		}	
+	}	
 }	
